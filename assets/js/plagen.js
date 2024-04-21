b448da59f1c1a02d8b2746bbee7840ebd3724745
ahmed zaky <aalsaeed@nadeer.com.sa>
2024-04-21 21:26:54 +0200

    updates

--- assets/js/plagen.js
+++ assets/js/plagen.js
@@ -279,4 +279,16 @@ $(function () {
     }
     window.addEventListener('load', navbarlinksActive);
     document.addEventListener('scroll', navbarlinksActive);
-  
+
+    
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      });
+    }
+    window.addEventListener('load', () => {
+      aos_init();
+    });
